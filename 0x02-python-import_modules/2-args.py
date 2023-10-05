539b4838492c361e768e81dc6de5f077feebd607
yousefayman <yousefayman20003@gmail.com>
2023-10-05 06:44:30 -0700

    up

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     n = len(argv)
     if n == 1:
         print("0 arguments.")
+    elif n == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
     else:
-        print("{} argument:".format(n - 1))
+        print("{} arguments:".format(n - 1))
         for i in range(1, n):
             print("{}: {}".format(i, argv[i]))
