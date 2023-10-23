1c0c75b3474e3508f4ef307c97c317957ad88194
yousefayman <yousefayman20003@gmail.com>
2023-10-23 07:15:43 -0700

    upl

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,6 +6,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         printed = True
-    except IndexError:
+    except (TypeError, ValueError):
         None
     return printed
