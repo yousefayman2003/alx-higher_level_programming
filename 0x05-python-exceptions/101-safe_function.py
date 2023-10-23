b14242147f4e0c00e270c4f40b22082def120571
yousefayman <yousefayman20003@gmail.com>
2023-10-23 07:54:17 -0700

    upload

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,7 @@ def safe_function(fct, *args):
 
     try:
         value = fct(*args)
+        return value
     except Exception as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return None
