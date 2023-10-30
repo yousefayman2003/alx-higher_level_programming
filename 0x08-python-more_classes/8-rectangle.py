a947ff82fc043f6e876784a2df76a8aea94b26a3
yousefayman <yousefayman20003@gmail.com>
2023-10-30 06:48:26 -0700

    Auto pushing

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -16,8 +16,8 @@ class Rectangle:
                 width (int): width of the rectangle.
                 height (int): height of the rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
